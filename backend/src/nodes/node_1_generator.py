208ce81f621a40acf2c4355185b43224242c3146
xCrepe <vinhhoangismyname@gmail.com>
2026-06-10 14:28:15 +0700

    Fix: node 1

--- backend/src/nodes/node_1_generator.py
+++ backend/src/nodes/node_1_generator.py
@@ -1,6 +1,6 @@
 from .base_node import BaseNode
-from ..schemas.node_1_schema import Node1Input, Node1Output
-from ..core.llm_client import llm
+from src.schemas.node_1_schema import Node1Input, Node1Output
+from src.core import llm_client
 import time
 
 class CodeGenerator(BaseNode):
@@ -8,27 +8,26 @@ class CodeGenerator(BaseNode):
 
     def __call__(self, state: Node1Input) -> Node1Output:
         start_time = time.perf_counter()
-
+        
         requirement = state["state"]["requirement"]
-
-        prompt = f"""You are a helpful assistant that generates Python code based on the given requirement.
-        Requirement: {requirement}
-
-        Please provide the generated code without any explanations or comments. The response should be purely the code that fulfills the requirement."""
-
-        response = llm.invoke(prompt)
-        # response = "```python\ndef sum_list(numbers):\n    return sum(numbers)\n```"
-        generated_code = response.content
-        # generated_code = response
-
-        new_state = state["state"].copy()
-        new_state["code"] = generated_code.split("```python")[1].split("```")[0].strip() if "```python" in generated_code else generated_code.strip()
-
-        action = {
-            "next_node": "code_executor",
-            "message": "Code generation completed. Proceeding to execution.",
-            "reasoning": "The code has been generated based on the requirement, and we can now execute it to see if it meets the requirement."
-        }
+        if state["state"].get("retry_count", 0) < 1:
+            code = self._new_code(requirement)
+            new_state = self._new_state(state, code)
+            action = {
+                "next_node": "code_executor",
+                "message": "Code generation completed. Proceeding to execution.",
+                "reasoning": "The code has been generated based on the requirement, and we can now execute it to see if it meets the requirement."
+            }
+        else:
+            code = state["state"].get("code", None)
+            stderr = state["state"]["execution_result"].get("stderr", None)
+            code = self._fix_code(requirement, code, stderr)
+            new_state = self._new_state(state, code)
+            action = {
+                "next_node": "code_executor",
+                "message": "Code has been revised based on the previous execution results. Proceeding to execution.",
+                "reasoning": "The code has been revised to address the errors encountered in the previous execution attempt. We will execute the revised code to check if it now meets the requirement."
+            }
 
         output = Node1Output(state=new_state, action=action)
 
@@ -38,3 +37,38 @@ class CodeGenerator(BaseNode):
         self.write_history(output, state, output, duration_ms=duration_ms)
 
         return output
+
+    def _new_state(self, state: Node1Input, code: str) -> Node1Input:
+        new_state = state["state"].copy()
+        new_state["code"] = code.split("```python")[1].split("```")[0].strip() if "```python" in code else code.strip()
+
+        return new_state
+
+    def _new_code(self, requirement: str) -> str:
+
+        prompt = f"""You are a helpful assistant that generates Python code based on the given requirement.
+        Requirement: {requirement}
+
+        Please provide the generated code without any explanations or comments. The response should be purely the code that fulfills the requirement."""
+
+        # response = llm_client.llm.invoke(prompt)
+        response = llm_client.groq.invoke(prompt)
+        return response.content
+
+    def _fix_code(self, requirement: str, code: str | None, stderr: str | None) -> dict:
+        if stderr and code:            
+            prompt = f"""The previously generated code did not pass the test. Please revise the code based on the following previous code, requirement and error message.
+            Requirement: {requirement}
+            Previous Code: ```python
+            {code}
+            ```
+            Error Message: {stderr}
+
+            Please provide the revised code without any explanations or comments. The response should be purely the code that fulfills the requirement and resolves the errors."""
+
+            # response = llm_client.llm.invoke(prompt)
+            response = llm_client.groq.invoke(prompt)
+            revised_code = response.content
+            return revised_code
+        else:
+            return code
